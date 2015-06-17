0117f4f53efdf220f381b55cfdb30de6a533f4d7
Matt Cleghorn <matt@madebymatt.co.uk>
2015-06-17 09:24:55 +0100

    Remove node modules, create gitignore

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
-var Backbone = require('backbone');
+/*var Backbone = require('backbone');
 var Hub = require('./hub');
 var AccordionView = require('js/views/accordionView');
 var AccordionModel = require('js/models/accordionModel');
+*/
 
 $(function() {
+
+	var allSections = $('.accordion-body').addClass('hidden');
+
+	$('.accordion-title').on('click', function(){
+		var accordionBody = $(this).next('.accordion-body');
+
+		accordionBody.toggleClass('visible');
+	})
 
 });
